fbeca2b681510bf614f3abdb27ff45ba9597dec2
Robert Mikhayelyan <rob.mikh@outlook.com>
2020-04-26 13:37:37 -0700

    basic window and visual tree

---
+++ src/interop.rs
@@ -0,0 +1,109 @@
+use std::ffi::c_void;
+use crate::windows::{
+    ui::composition::{
+        CompositionTarget,
+        desktop::{
+            DesktopWindowTarget,
+        },
+    },
+    system::DispatcherQueueController,
+};
+
+#[repr(C)]
+struct abi_ICompositorDesktopInterop {
+    __base : [usize; 3],
+    create_desktop_window_target: extern "system" fn(winrt::RawPtr, *mut c_void, i32, *mut winrt::RawPtr) -> winrt::ErrorCode,
+    ensure_on_thread: extern "system" fn(winrt::RawPtr, u32) -> winrt::ErrorCode,
+}
+
+unsafe impl winrt::ComInterface for CompositorDesktopInterop {
+    const GUID: winrt::Guid = winrt::Guid::from_values(
+        702976506, 17767, 19914, [179, 25, 208, 242, 7, 235, 104, 7]
+    );
+}
+
+#[repr(transparent)]
+#[derive(Default, Clone)]
+pub struct CompositorDesktopInterop {
+    ptr: winrt::IUnknown,
+}
+
+impl CompositorDesktopInterop {
+    pub fn create_desktop_window_target(&self, hwnd: *mut c_void, is_topmost: bool) -> winrt::Result<DesktopWindowTarget> {
+        unsafe {
+            let mut __ok = std::mem::zeroed();
+            ((*(*(self.ptr.get() as *const *const abi_ICompositorDesktopInterop))).create_desktop_window_target)(self.ptr.get(), hwnd, is_topmost as i32, &mut __ok)
+                .and_then(|| {
+                    let result = std::mem::transmute_copy(&__ok);
+                    std::mem::forget(__ok);
+                    result
+                })
+        }
+    }
+}
+
+#[link(name = "windowsapp")]
+extern "stdcall" {
+    fn RoInitialize(
+        init_type: i32,
+    ) -> winrt::ErrorCode;
+}
+
+#[repr(i32)]
+pub enum RoInitType {
+    MultiThreaded = 0,
+    SingleThreaded = 1,
+}
+
+pub fn ro_initialize(init_type: RoInitType) -> winrt::Result<()> {
+    unsafe {
+        RoInitialize(std::mem::transmute(init_type)).ok()
+    }
+}
+
+#[link(name = "coremessaging")]
+extern "stdcall" {
+    fn CreateDispatcherQueueController(
+        options: DispatcherQueueOptions,
+        dispatcherQueueController: *mut winrt::RawPtr,
+    ) -> winrt::ErrorCode;
+}
+
+#[repr(C)]
+struct DispatcherQueueOptions {
+    size: u32,
+    thread_type: i32,
+    apartment_type: i32,
+}
+
+#[repr(i32)]
+pub enum DispatcherQueueThreadType {
+    Dedicated = 1,
+    Current = 2,
+}
+
+#[repr(i32)]
+pub enum DispatcherQueueThreadApartmentType {
+    None = 0,
+    ASTA = 1,
+    STA = 2,
+}
+
+pub fn create_dispatcher_queue_controller(thread_type: DispatcherQueueThreadType, apartment_type: DispatcherQueueThreadApartmentType) -> winrt::Result<DispatcherQueueController> {
+    unsafe {
+        let options = DispatcherQueueOptions {
+            size: std::mem::size_of::<DispatcherQueueOptions>() as u32,
+            thread_type: std::mem::transmute(thread_type),
+            apartment_type: std::mem::transmute(apartment_type),
+        };
+        let mut ptr = std::mem::zeroed();
+        CreateDispatcherQueueController(options, &mut ptr).ok()?;
+        let result = std::mem::transmute_copy(&ptr);
+        std::mem::forget(ptr);
+        Ok(result)
+    }
+}
+
+pub fn create_dispatcher_queue_controller_for_current_thread() -> winrt::Result<DispatcherQueueController> {
+    create_dispatcher_queue_controller(DispatcherQueueThreadType::Current, DispatcherQueueThreadApartmentType::None)
+}
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+winrt::import!(
+    dependencies
+        "os"
+    modules
+        "windows.foundation.collections"
+        "windows.foundation.numerics"
+        "windows.ui"
+        "windows.ui.composition"
+        "windows.ui.composition.desktop"
+        "windows.graphics"
+        "windows.system"
+);
+
+mod interop;
+
+use interop::{ro_initialize, RoInitType, CompositorDesktopInterop, create_dispatcher_queue_controller_for_current_thread};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use raw_window_handle::HasRawWindowHandle;
+
+use winrt::TryInto;
+use windows::{
+    foundation::{
+        numerics::{Vector2, Vector3,},
+    },
+    ui::{
+        Colors,
+        composition::{
+            Compositor,
+        },
+    },
+};
+
+fn run() -> winrt::Result<()> {
+    ro_initialize(RoInitType::MultiThreaded)?;
+    let _controller = create_dispatcher_queue_controller_for_current_thread()?;
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    window.set_title("Minesweeper");
+
+    // Get the window handle
+    let window_handle = window.raw_window_handle();
+    let window_handle = match window_handle {
+        raw_window_handle::RawWindowHandle::Windows(window_handle) => window_handle.hwnd,
+        _ => panic!("Unsupported platform!"),
+    };
+
+    let compositor = Compositor::new()?;
+    let compositor_desktop: CompositorDesktopInterop = compositor.try_into().unwrap();
+    let target = compositor_desktop.create_desktop_window_target(window_handle, false)?;
+
+    let root = compositor.create_sprite_visual()?;
+    root.set_relative_size_adjustment(Vector2{ x: 1.0, y: 1.0 })?;
+    let brush = compositor.create_color_brush_with_color(Colors::white()?)?;
+    root.set_brush(brush)?;
+    target.set_root(&root)?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                // TODO: resize
+            },
+            _ => (),
+        }
+    });
+
+    Ok(())
+}
+
+fn main() {
+    let result = run();
+
+    let code = match result {
+        Ok(_) => winrt::ErrorCode(0),
+        Err(code) => code.code(),
+    };
+    code.unwrap();
+}
