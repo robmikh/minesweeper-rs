c9e2462022c388f659b9ab3e6bff4658750f65f9
Robert Mikhayelyan <rob.mikh@outlook.com>
2020-04-30 14:30:49 -0700

    cleanup window target creation

---
+++ src/window_target.rs
@@ -0,0 +1,34 @@
+use crate::interop::CompositorDesktopInterop;
+use crate::windows::ui::composition::{desktop::DesktopWindowTarget, Compositor};
+use raw_window_handle::HasRawWindowHandle;
+use winrt::TryInto;
+
+pub trait CompositionDesktopWindowTargetSource {
+    fn create_window_target(
+        &self,
+        compositor: &Compositor,
+        is_topmost: bool,
+    ) -> winrt::Result<DesktopWindowTarget>;
+}
+
+impl<T> CompositionDesktopWindowTargetSource for T
+where
+    T: HasRawWindowHandle,
+{
+    fn create_window_target(
+        &self,
+        compositor: &Compositor,
+        is_topmost: bool,
+    ) -> winrt::Result<DesktopWindowTarget> {
+        // Get the window handle
+        let window_handle = self.raw_window_handle();
+        let window_handle = match window_handle {
+            raw_window_handle::RawWindowHandle::Windows(window_handle) => window_handle.hwnd,
+            _ => panic!("Unsupported platform!"),
+        };
+
+        let compositor_desktop: CompositorDesktopInterop = compositor.try_into()?;
+        let target = compositor_desktop.create_desktop_window_target(window_handle, is_topmost)?;
+        Ok(target)
+    }
+}
