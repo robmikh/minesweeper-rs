3476c563a7ca6d3e198e9b64dccbedd5fd42ef06
Robert Mikhayelyan <rob.mikh@outlook.com>
2020-05-17 20:11:46 -0700

    fix other board sizes

--- src/minesweeper.rs
+++ src/minesweeper.rs
@@ -100,7 +100,7 @@ impl Minesweeper {
             game_over: false,
         };
 
-        result.new_game(16, 16, 40)?;
+        result.new_game(game_board_size_in_tiles.width, game_board_size_in_tiles.height, 40)?;
         result.on_parent_size_changed(parent_size)?;
 
         Ok(result)
