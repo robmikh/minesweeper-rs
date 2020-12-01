6e90bcdd041c0ee73c31b3e2de76ea466f41883b
Kenny Kerr <kenny.kerr@microsoft.com>
2020-12-01 08:58:48 -0800

    windows

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,23 +1,22 @@
 fn main() {
     winrt::build!(
-        types
-            windows::foundation::numerics::{Vector2, Vector3}
-            windows::foundation::TimeSpan
-            windows::graphics::SizeInt32
-            windows::system::DispatcherQueueController
-            windows::ui::composition::{
-                AnimationIterationBehavior,
-                CompositionBatchTypes,
-                CompositionBorderMode,
-                CompositionColorBrush,
-                CompositionGeometry,
-                CompositionShape,
-                CompositionSpriteShape,
-                Compositor,
-                ContainerVisual,
-                SpriteVisual,
-            }
-            windows::ui::composition::desktop::DesktopWindowTarget
-            windows::ui::Colors
+        windows::foundation::numerics::{Vector2, Vector3}
+        windows::foundation::TimeSpan
+        windows::graphics::SizeInt32
+        windows::system::DispatcherQueueController
+        windows::ui::composition::{
+            AnimationIterationBehavior,
+            CompositionBatchTypes,
+            CompositionBorderMode,
+            CompositionColorBrush,
+            CompositionGeometry,
+            CompositionShape,
+            CompositionSpriteShape,
+            Compositor,
+            ContainerVisual,
+            SpriteVisual,
+        }
+        windows::ui::composition::desktop::DesktopWindowTarget
+        windows::ui::Colors
     );
 }
