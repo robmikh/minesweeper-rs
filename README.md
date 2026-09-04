673ee0d3e495ad531f833bc850dae9a0fc58f5c8
Kenny Kerr <kenny@kennykerr.ca>
2026-09-03 19:27:10 -0500

    simpler

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # minesweeper-rs
 A port of [robmikh/Minesweeper](https://github.com/robmikh/Minesweeper) using the
-[`windows-composition`](https://crates.io/crates/windows-composition) and
-[`windows-window`](https://crates.io/crates/windows-window) crates from
-[windows-rs](https://github.com/microsoft/windows-rs).
+[`windows-composition`](https://github.com/microsoft/windows-rs/tree/fa5059c3e7462ce66982ff7b9c523fae55b9d189/crates/libs/composition)
+and [`windows-window`](https://github.com/microsoft/windows-rs/tree/fa5059c3e7462ce66982ff7b9c523fae55b9d189/crates/libs/window)
+crates from [windows-rs PR #4879](https://github.com/microsoft/windows-rs/pull/4879).
 
 ## Running
 Running this sample requires at least Windows build 1803 (v10.0.17134.0). To compile and run (after setting up), use:
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,17 @@ mod visual_grid;
 
 use minesweeper::Minesweeper;
 use std::cell::RefCell;
-use std::ffi::c_void;
 use std::rc::Rc;
 use windows_composition::{Compositor, DispatcherQueueController, Result, Vector2};
 use windows_window::Window;
 
 const WINDOW_CLIENT_WIDTH: i32 = 800;
 const WINDOW_CLIENT_HEIGHT: i32 = 600;
-const WS_OVERLAPPEDWINDOW: u32 = 0x00CF_0000;
-const WS_EX_NOREDIRECTIONBITMAP: u32 = 0x0020_0000;
 const WM_MOUSEMOVE: u32 = 0x0200;
 const WM_LBUTTONDOWN: u32 = 0x0201;
 const WM_RBUTTONDOWN: u32 = 0x0204;
 
 fn main() -> Result<()> {
-    preserve_legacy_dpi_behavior();
-
     let _queue = DispatcherQueueController::create_on_current_thread()?;
     let compositor = Compositor::new()?;
 
@@ -36,11 +31,9 @@ fn main() -> Result<()> {
     let window = {
         let game_message = game.clone();
         let game_resize = game.clone();
-        let (window_width, window_height) =
-            outer_size_for_client_size(WINDOW_CLIENT_WIDTH, WINDOW_CLIENT_HEIGHT)?;
         Window::new("Minesweeper")
-            .size(window_width, window_height)
-            .ex_style(WS_EX_NOREDIRECTIONBITMAP)
+            .client_size(WINDOW_CLIENT_WIDTH, WINDOW_CLIENT_HEIGHT)
+            .no_redirection_bitmap()
             .on_message(move |_hwnd, message, _wparam, lparam| {
                 if let Some(game) = game_message.borrow_mut().as_mut() {
                     match message {
@@ -76,41 +69,4 @@ fn main() -> Result<()> {
 
 fn point_from_lparam(lparam: isize) -> Vector2 {
     Vector2::new((lparam as i16) as f32, ((lparam >> 16) as i16) as f32)
-}
-
-fn preserve_legacy_dpi_behavior() {
-    const DPI_AWARENESS_CONTEXT_UNAWARE: *mut c_void = -1_isize as *mut c_void;
-
-    _ = unsafe { SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_UNAWARE) };
-}
-
-fn outer_size_for_client_size(width: i32, height: i32) -> Result<(i32, i32)> {
-    let mut rect = Rect {
-        left: 0,
-        top: 0,
-        right: width,
-        bottom: height,
-    };
-
-    if unsafe { AdjustWindowRectEx(&mut rect, WS_OVERLAPPEDWINDOW, 0, WS_EX_NOREDIRECTIONBITMAP) }
-        == 0
-    {
-        Err(windows_core::Error::from_thread())
-    } else {
-        Ok((rect.right - rect.left, rect.bottom - rect.top))
-    }
-}
-
-#[repr(C)]
-struct Rect {
-    left: i32,
-    top: i32,
-    right: i32,
-    bottom: i32,
-}
-
-#[link(name = "user32")]
-extern "system" {
-    fn AdjustWindowRectEx(rect: *mut Rect, style: u32, has_menu: i32, ex_style: u32) -> i32;
-    fn SetProcessDpiAwarenessContext(value: *mut c_void) -> i32;
 }
